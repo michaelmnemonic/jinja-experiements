ab91517db920da89a24e52bc54a347a18fe68d80
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-15 16:39:23 +0200

    Construct an example deployment

--- generate-yaml.py
+++ generate-yaml.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
+import yaml
 
 def main():
     env = Environment(
@@ -6,6 +7,14 @@ def main():
         autoescape=select_autoescape()
     )
 
+    # construct example deployment
+    example_deployment = {
+        "name": "Example deployment",
+        "description": "This is an example deployment to test jinja templates",
+        "functions": ["read_input_data", "validate_raw_input"]}
+
+    template = env.get_template("deployment.yaml.j2")
+    print(template.render(example_deployment))
 
 if __name__ == "__main__":
     main()
