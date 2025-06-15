14b81f9fac2334132d75d93c676b19c3a56166c5
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-15 15:56:38 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # Define 'forAllSystems' for properties that shall be build for x86_64 *and* aarch64
+    systems = ["x86_64-linux" "aarch64-linux"];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+  in {
+    devShells = forAllSystems (system: {
+      default = pkgs.${system}.mkShell {
+        buildInputs = with pkgs.${system}; [
+          gitMinimal
+          nil
+          alejandra
+        ];
+      };
+    });
+  };
+}
