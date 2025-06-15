3144293a1c1a21fa721da8779e4aef288181087c
michaelmnemonic <michaelmnemonic@posteo.eu>
2025-06-15 16:08:25 +0200

    Follow jinja API Basics

--- generate-yaml.py
+++ generate-yaml.py
@@ -1,6 +1,11 @@
-import jinja2
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 def main():
+    env = Environment(
+        loader=PackageLoader("generate-yaml"),
+        autoescape=select_autoescape()
+    )
+
 
 if __name__ == "__main__":
     main()
